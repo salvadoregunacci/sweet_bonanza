ced197693b773c95dbe3688c7828e3702913db90
Salvadore <salvadoregunacci5@gmail.com>
2023-03-23 15:12:22 +0200

    закончил sticky блок, начал адаптив

--- scripts/brand.js
+++ scripts/brand.js
@@ -2,18 +2,42 @@
 // Variables
 // =========================
 
+// $ - element from DOM
+
 const $selects = document.querySelectorAll('.select');
 const $replyReviews = document.querySelectorAll('.review[reply]');
+const $replyButtons = document.querySelectorAll('.review__btn');
+const $commentArea = document.querySelector('.review__txt');
+const $reviewForm = document.querySelector('.review__form');
+const $reviewType = document.querySelector('.review__type');
+const $replyId = document.querySelector('.review__reply_id');
+
+const $promoSection = document.querySelector('#promo');
+const $stickyBlock = document.querySelector('#sticky');
+const $stickyCloseBtn = document.querySelector('.sticky__close_btn');
+
+const observer = new IntersectionObserver(stickyVisibleHandler);
 
 // =========================
 // Events
 // =========================
+
+if ($promoSection) {
+  observer.observe($promoSection);
+}
+
+if ($stickyBlock && $stickyCloseBtn) {
+  $stickyCloseBtn.addEventListener("click", ()=> {
+    $stickyBlock.removeAttribute("active");
+  })
+}
 
 if ($selects) {
   $selects.forEach(select => {
     select.addEventListener("click", selectShowController);
   });
 }
+
 if ($replyReviews) {
   $replyReviews.forEach(reply => {
     const $replyName = reply.querySelector(".review__reply span");
@@ -24,11 +48,54 @@ if ($replyReviews) {
   });
 }
 
+if ($replyButtons){
+  $replyButtons.forEach(btn => {
+    btn.addEventListener("click", replyClick);
+  });
+}
+
+if ($commentArea) {
+  $commentArea.addEventListener("input", ()=> {
+    if ($commentArea.value.length <= 0) {
+      $reviewForm.removeAttribute("is_reply");
+
+      if ($reviewType && $replyId) {
+        $reviewType.value = "comment";
+        $replyId.value = "";
+      }
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", checkOverflowFetures);
 
 // =========================
 // Functions
 // =========================
+
+function stickyVisibleHandler(entries) {
+  if (entries[0].isIntersecting) {
+    $stickyBlock.removeAttribute("active");
+  } else {
+    $stickyBlock.setAttribute("active", "");
+  }
+}
+
+function replyClick(e){
+  const eT = e.target;
+  const $curReply = eT.closest(".review");
+  let $replyName = $curReply ? $curReply.querySelector(".review__name") : "";
+
+  if ($replyName && $commentArea && $reviewForm && $reviewType && $replyId) {
+    $replyName = $replyName.textContent.trim();
+    $commentArea.value = `${$replyName}, `;
+    $commentArea.focus();
+    $reviewForm.setAttribute("is_reply", "");
+    $commentArea.scrollIntoView();
+    $reviewType.value = "reply";
+    $replyId.value = $curReply.getAttribute("review_id");
+  }
+}
 
 function selectShowController(e) {
   const eT = e.target.closest(".select");
