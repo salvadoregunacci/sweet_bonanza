23033e898c6ee70a4c6d4c6b143b3b23a6e2221a
Salvadore <salvadoregunacci5@gmail.com>
2023-03-24 19:23:31 +0200

    осталось только сео

--- scripts/brand.js
+++ scripts/brand.js
@@ -22,12 +22,14 @@ const observer = new IntersectionObserver(stickyVisibleHandler);
 // Events
 // =========================
 
+window.addEventListener("resize", checkTabletDescTable);
+
 if ($promoSection) {
   observer.observe($promoSection);
 }
 
 if ($stickyBlock && $stickyCloseBtn) {
-  $stickyCloseBtn.addEventListener("click", ()=> {
+  $stickyCloseBtn.addEventListener("click", () => {
     $stickyBlock.removeAttribute("active");
   })
 }
@@ -48,14 +50,14 @@ if ($replyReviews) {
   });
 }
 
-if ($replyButtons){
+if ($replyButtons) {
   $replyButtons.forEach(btn => {
     btn.addEventListener("click", replyClick);
   });
 }
 
 if ($commentArea) {
-  $commentArea.addEventListener("input", ()=> {
+  $commentArea.addEventListener("input", () => {
     if ($commentArea.value.length <= 0) {
       $reviewForm.removeAttribute("is_reply");
 
@@ -67,11 +69,64 @@ if ($commentArea) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", checkOverflowFetures);
+document.addEventListener("DOMContentLoaded", () => {
+  checkOverflowFetures();
+  checkTabletDescTable();
+});
 
 // =========================
 // Functions
 // =========================
+
+function checkTabletDescTable() {
+  if (window.innerWidth < 768) {
+    const $descTables = document.querySelectorAll('.db_table');
+
+    if ($descTables && $descTables.length > 0) {
+      $descTables.forEach(table => {
+        _setTabletDescTable(table);
+      });
+    }
+  }
+
+  if (window.innerWidth > 768) {
+    _removeTabletTables();
+  }
+
+  if (window.innerWidth <= 390) {
+    _removeTabletTables();
+  }
+}
+
+function _removeTabletTables() {
+  const $oldTables = document.querySelectorAll('.db_table__tablet');
+  if (!$oldTables.length) return;
+
+  $oldTables.forEach(table => table.remove());
+}
+
+function _setTabletDescTable(curTable) {
+  const $oldTables = document.querySelectorAll('.db_table__tablet');
+  const $curTables = document.querySelectorAll('.db_table');
+
+  if ($oldTables.length === ($curTables.length - $oldTables.length)) return;
+
+  const table = document.createElement("table");
+  table.classList.add("db_table", "db_table__tablet");
+  curTable.after(table);
+
+  let rows = Array.from(curTable.rows);
+
+  rows.forEach(row => {
+    const tr_1 = document.createElement("tr");
+    const tr_2 = document.createElement("tr");
+
+    tr_1.append(row.cells[0].cloneNode(true), row.cells[1].cloneNode(true));
+    tr_2.append(row.cells[2].cloneNode(true), row.cells[3].cloneNode(true));
+
+    table.append(tr_1, tr_2);
+  });
+}
 
 function stickyVisibleHandler(entries) {
   if (entries[0].isIntersecting) {
@@ -81,7 +136,7 @@ function stickyVisibleHandler(entries) {
   }
 }
 
-function replyClick(e){
+function replyClick(e) {
   const eT = e.target;
   const $curReply = eT.closest(".review");
   let $replyName = $curReply ? $curReply.querySelector(".review__name") : "";
